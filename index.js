e7f0826473b47078e3088f311df9e576f7c60366
dennis00095 <dennis.mwongela@student.moringaschool.com>
2022-08-23 00:47:15 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(distance ){
+    if(distance > 42) { 
+        return distance - 42 
+    }else {
+        return 42 - distance;
+    }
+}
+function distanceFromHqInFeet(distanceInFeet){
+   
+    return distanceFromHqInBlocks(distanceInFeet)* 264;
+}
+function distanceTravelledInFeet(x , y) { //start of the travel
+    let totalDistancexy;
+    if (y > x){
+        totalDistancexy = y - x
+    }else{
+        totalDistancexy = x - y
+    }
+    return totalDistancexy * 264;
+}
+function calculatesFarePrice(start, destination){
+    let totalDistancexy = distanceTravelledInFeet(start, destination)
+    let price;
+    if (totalDistancexy <= 400){
+        return price = 0;
+    } else if (totalDistancexy > 400  && (totalDistancexy <= 2000)){
+        return price = 2.56;
+    }else if(totalDistancexy > 2000 && (totalDistancexy <= 2500)){
+        return price = 25;
+    }else{
+        return price = `cannot travel that far`;
+    }
+}
